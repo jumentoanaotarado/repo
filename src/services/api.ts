e92a98ca9f8b8ea6f9a7b6026402d3ce99b4efe2
jumentoanaotarado <pawpatinho@gmail.com>
2024-06-05 11:11:00 -0300

    next.config.js

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,21 @@
 import axios from "axios";
+import fetch from 'node-fetch';
 
 
 export const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
 })
+
+export default async function handler(request, response) {
+    const res = await fetch('https://...', {
+      method: 'POST',
+      body: JSON.stringify({
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  
+    const data = await res.json();
+    return response.status(200).json({ data });
+  }

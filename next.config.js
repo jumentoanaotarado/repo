fe8d20d42a7c74072b5d03b0b37ead00c14379cb
jumentoanaotarado <pawpatinho@gmail.com>
2024-08-06 03:11:14 -0300

    kaopkspo

--- next.config.js
+++ next.config.js
@@ -6,3 +6,12 @@ const nextConfig = {
 }
  
 module.exports = nextConfig
+
+
+modulemodule.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
